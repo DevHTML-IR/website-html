73ade01d43af52e86273f549b82ff92ffb4d8ba9
DevHTML-IR <film.website0@gmail.com>
2023-05-15 09:23:07 +0200

    Initial Commit

---
+++ condition.js
@@ -0,0 +1,34 @@
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+
+  var tempDiv = document.createElement("div");
+  tempDiv.style.position = "absolute";
+  tempDiv.style.left = e.clientX + "px";
+  tempDiv.style.top = e.clientY + "px";
+  tempDiv.style.fontSize = "20px";
+  tempDiv.style.transform = "translate(-50%, -100%)";
+  tempDiv.style.zIndex = "9999";
+
+  document.body.appendChild(tempDiv);
+
+  setTimeout(function () {
+    tempDiv.remove();
+  }, 1000);
+}, false);
+
+document.addEventListener("keydown", function (e) {
+  e.preventDefault();
+});
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('selectstart', function (e) {
+    e.preventDefault();
+  });
+});
+function changeLanguage(selectElement) {
+  var selectedValue = selectElement.value;
+  if (selectedValue === 'en') {
+    window.location.href = '';
+  } else if (selectedValue === 'fr') {
+    window.location.href = ''; 
+  }
+}
