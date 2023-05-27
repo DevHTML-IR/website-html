426046cb1f8a355ab927ce7951e470f6c1f1d0b8
DevHTML-IR <film.website0@gmail.com>
2023-05-27 17:38:33 +0200

    Add Cookie

--- condition.js
+++ condition.js
@@ -33,3 +33,62 @@ function changeLanguage(selectElement) {
     window.location.href = ''; 
   }
 }
+// Vérifier si l'utilisateur a déjà fait un choix
+var cookieChoice = getCookie('cookieChoice');
+
+if (!cookieChoice) {
+  // Afficher la bannière si aucun choix n'a été enregistré
+  showBanner();
+}
+
+// Gérer le clic sur le bouton "Accepter"
+document.getElementById('acceptButton').addEventListener('click', function() {
+  // Définir le cookie pour enregistrer le choix d'acceptation
+  setCookie('cookieChoice', 'accepted', 365);
+  hideBanner();
+});
+
+// Gérer le clic sur le bouton "Refuser"
+document.getElementById('refuseButton').addEventListener('click', function() {
+  // Définir le cookie pour enregistrer le choix de refus
+  setCookie('cookieChoice', 'refused', 365);
+  hideBanner();
+});
+
+// Fonction pour afficher la bannière
+function showBanner() {
+  document.getElementById('cookieBanner').style.display = 'block';
+}
+
+// Fonction pour masquer la bannière
+function hideBanner() {
+  document.getElementById('cookieBanner').style.display = 'none';
+}
+
+// Fonction pour obtenir la valeur d'un cookie
+function getCookie(name) {
+  var cookieName = name + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var cookieArray = decodedCookie.split(';');
+  for (var i = 0; i < cookieArray.length; i++) {
+    var cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return '';
+}
+
+// Fonction pour définir un cookie
+function setCookie(name, value, days) {
+  var expires = '';
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
